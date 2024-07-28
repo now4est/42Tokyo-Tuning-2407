e1d39350d95f911a964e9e5a14d8a96193e3754b
kotto5 <kotto555555@gmail.com>
2024-07-28 11:43:25 +0900

    location index

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -2,3 +2,5 @@
 
 CREATE INDEX idx_edges_node_a_id ON edges(node_a_id);
 CREATE INDEX idx_nodes_area_id ON nodes(area_id);
+
+CREATE INDEX idx_locations_tow_truck_id_timestamp ON locations(tow_truck_id, timestamp);
